b1b6ec46ef177e81246ebd204ead08da79a75af2
Jakson Reis <jakson.reis@hotmail.com>
2022-11-08 08:46:13 -0300

    implement local strategy validation

---
+++ src/user/user.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+/* import { UpdateUserDto } from './dto/update-user.dto'; */
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService){}
+
+
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10)
+    }
+
+    const createdUser = await this.prisma.user.create({ data });
+
+    return {
+      ...createdUser,
+      password: undefined,
+    };
+  }
+
+  findByEmail(email: string) {
+    
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+/* 
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  } */
+}
