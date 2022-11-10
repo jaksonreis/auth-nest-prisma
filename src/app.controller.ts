0916c5a5af255579c75300615b52a319e7badc5d
Jakson Reis <jakson.reis@hotmail.com>
2022-11-10 16:32:34 -0300

    add archive necessary

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
+import { User } from './user/entities/user.entity';
 
 @Controller()
 export class AppController {
@@ -9,4 +11,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+
+  @Get('me')
+  getMe(@CurrentUser() user: User) {
+    return user;
+  }
+
 }
